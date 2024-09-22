f3010780b89bafde6e849dedb31694456b2851c6
Deepak Kumar Yadav <yadav1996deepak1@gmail.com>
2024-09-22 19:52:32 +0530

    Update ColorPair_test_main.c

--- ColorPair_test_main.c
+++ ColorPair_test_main.c
@@ -1,6 +1,8 @@
 #include "Test_ColorPair.h"
+#include "ColorPair_definitions.h"
 
 int main() {
-    runColorPairTests(); 
+    runColorPairTests();          // Run all tests
+    printColorCodeManual();       // Print the reference manual
     return 0;
 }
